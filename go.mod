4dfe0e28beb31c7a95186e6b9000aeb27e515bb8
scw007 <steven.whitehead007@gmail.com>
2022-01-07 23:58:42 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/scw007/intro
+
+go 1.17
+
+require (
+	github.com/bwmarrin/dca v0.0.0-20180611192139-99fdf68b87a1 // indirect
+	github.com/bwmarrin/discordgo v0.23.2 // indirect
+	github.com/gorilla/websocket v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
+	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
+)
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var sounds = map[string][][]byte{}
+
+func main() {
+	var token string
+	flag.StringVar(&token, "t", "", "Bot Token")
+	flag.Parse()
+
+	// load all the sounds
+	log.Print("loading sounds")
+	configData, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := map[string]string{}
+	err = json.Unmarshal(configData, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for key, value := range config {
+		sounds[key] = loadSound(value)
+	}
+	// connect to discord
+	log.Print("joining discord")
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer discord.Close()
+
+	log.Print("adding handler")
+	discord.Identify.Intents = discordgo.IntentsGuildVoiceStates
+	discord.AddHandler(serverJoin)
+
+	log.Print("creating websocket")
+	err = discord.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("waiting for close")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
+
+func loadSound(path string) [][]byte {
+	buffer := [][]byte{}
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var opuslen int16
+	for {
+		err = binary.Read(file, binary.LittleEndian, &opuslen)
+		// If this is the end of the file, just return.
+		if err == io.EOF || err == io.ErrUnexpectedEOF {
+			err := file.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+			break
+		}
+
+		if err != nil {
+			fmt.Println("Error reading from dca file :", err)
+			log.Fatal(err)
+		}
+
+		// Read encoded pcm from dca file.
+		InBuf := make([]byte, opuslen)
+		err = binary.Read(file, binary.LittleEndian, &InBuf)
+
+		// Should not be any end of file errors
+		if err != nil {
+			fmt.Println("Error reading from dca file :", err)
+			log.Fatal(err)
+		}
+
+		// Append encoded pcm data to the buffer.
+		buffer = append(buffer, InBuf)
+	}
+
+	return buffer
+}
+
+func serverJoin(s *discordgo.Session, m *discordgo.VoiceStateUpdate) {
+	log.Print("event received")
+	if m.UserID == s.State.User.ID {
+		return
+	}
+	if !(m.BeforeUpdate == nil && m.ChannelID != "") {
+		return
+	}
+	buffer, ok := sounds[m.UserID]
+	if !ok {
+		log.Print("could not find user", m.UserID)
+		return
+	}
+	voice, err := s.ChannelVoiceJoin(m.GuildID, m.ChannelID, false, false)
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+	err = voice.Speaking(true)
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+	for _, buff := range buffer {
+		voice.OpusSend <- buff
+	}
+
+	err = voice.Speaking(false)
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+	voice.Disconnect()
+}
